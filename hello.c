f79451759f85d513232d3c5c1bc820ea90776712
Ngan Nguyen <nnguyen109@huskers.unl.edu>
2021-08-26 13:56:25 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main (int argc, char **argv) {
+     
+     printf("Hello World, My name is Ngan Nguyen!\n");
+     
+     printf("My major is Computer Science.\n");
+     
+     return 0;
+ }
