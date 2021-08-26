a5f5aadbc698d0f9a7eb7bb9016cb2c4b47178c6
Vyn3153 <89426671+Vyn3153@users.noreply.github.com>
2021-08-26 13:47:41 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,11 +4,9 @@
  int main (int argc, char **argv) {
      
      printf("Hello World, My name is Ngan Nguyen!\n");
-     
      printf("My major is Computer Science.\n");
   
      prinf("Hello World, My name is Miranda Lopez!\n");
-  
      print("My major is Computer Science.\n");
     
      printf("confusing");
