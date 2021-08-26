b74df6d3cc700ec8aa09afb884d81907d65f2e22
miranda-lopez <79167230+miranda-lopez@users.noreply.github.com>
2021-08-26 13:36:38 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,6 +6,10 @@
      printf("Hello World, My name is Ngan Nguyen!\n");
      
      printf("My major is Computer Science.\n");
+  
+  prinf("Hello World, My name is Miranda Lopez!\n");
+  
+  print("My major is Computer Science.\n");
      
      return 0;
  }
