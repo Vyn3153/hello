8487b08f98bebabc5d8efa161200036f781ef52e
Ngan Nguyen <nnguyen109@huskers.unl.edu>
2021-08-27 13:11:46 +0000

    update hello

--- hello.c
+++ hello.c
@@ -4,15 +4,18 @@
  *
  * Print name and major with a collaborator 
  */
+ 
 #include <stdlib.h>
 #include <stdio.h>
 
  int main (int argc, char **argv) {
      
+     //print name and major
      printf("Hello World, My name is Ngan Nguyen!\n");
      printf("My major is Computer Science.\n");
   
-     prinf("Hello World, My name is Miranda Lopez!\n");
+     //print collab name and major 
+     printf("Hello World, My name is Miranda Lopez!\n");
      printf("My major is Computer Science.\n");
      
      return 0;
